f8d28a97b1c3f5ebabacd35ecce857d5332d2f98
Swapneswarnayak <1705835@kiit.ac.in>
2024-03-21 13:45:12 +0530

    path dependency added on useEffect

--- src/contexts/JWTContext/AuthContext.provider.tsx
+++ src/contexts/JWTContext/AuthContext.provider.tsx
@@ -102,7 +102,7 @@ export default function AuthProvider({ children }: AuthProviderProps) {
     };
 
     initialize();
-  }, []);
+  }, [path]);
 
   const checkSessionExist = async (xml: any) => {
     try {
